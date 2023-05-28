258a0b8face30796986b380e3fcd5aba308111e2
krizhnaa <krizhnaprem@gmail.com>
2023-05-28 19:50:48 +0530

    Fixed the JS for better consistency!

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
-async function getJoke(){
+let getJoke = async () => {
+    jokeContainer.classList.remove("fade");
     const response = await fetch(url);
     const item = await response.json();
     jokeContainer.textContent = `${item.joke}`;
-    console.log(item.joke)
+    jokeContainer.classList.add("fade");
 };
+btn.addEventListener("click", getJoke);
 getJoke();
