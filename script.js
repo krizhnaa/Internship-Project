2de3c0f94aae405272e842430f91e168f18e1ced
krizhnaa <krizhnaprem@gmail.com>
2023-05-28 19:41:17 +0530

    Testing the Async function with API

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+async function getJoke(){
+    const response = await fetch(url);
+    const item = await response.json();
+    jokeContainer.textContent = `${item.joke}`;
+    console.log(item.joke)
+};
+getJoke();
